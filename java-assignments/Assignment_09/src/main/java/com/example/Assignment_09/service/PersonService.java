b79b4fdc62c0edc04cd0829f332d1ad8e3422b71
MaazMustafa <maaz.mustafa@joshsoftware.com>
2023-08-22 12:55:45 +0530

    Assinment number 10

--- java-assignments/Assignment_09/src/main/java/com/example/Assignment_09/service/PersonService.java
+++ java-assignments/Assignment_09/src/main/java/com/example/Assignment_09/service/PersonService.java
@@ -1,29 +1,20 @@
 package com.example.Assignment_09.service;
 
-import com.example.Assignment_09.dao.PersonRepository;
+import com.example.Assignment_09.dto.OuterPersonDTO;
 import com.example.Assignment_09.entity.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class PersonService {
-    @Autowired
-    private PersonRepository personRepository;
 
-    public Person getPersonById(int id){
-         Optional<Person> person=personRepository.findById(id);
-         if(person.isPresent()){
-             return person.get();
-         }
-         else {
-             return null;
-         }
-    }
+public interface PersonService {
 
-    public List<Person> getallPersons(){
-        return personRepository.findAll();
-    }
+
+    Person getPersonById(int id);
+
+    List<Person> getallPersons();
+
+    boolean savePerson(OuterPersonDTO person);
+
+    boolean deletePerson(int id);
+    Person updatePerson(Person person);
 }
